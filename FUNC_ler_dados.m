ea47d3a8bc93d2858abf4764c667bde76ca72247
Frutninja <45978599+Frutninja@users.noreply.github.com>
2019-01-09 13:14:15 +0000

    Update FUNC_ler_dados.m

--- FUNC_ler_dados.m
+++ FUNC_ler_dados.m
@@ -4,11 +4,11 @@ function [velocidade_mediana, thrust_efetivo] = ler_dados( file_name )
    
     %Leitura dos valores de DRAG - Thrust a 0%, apenas no inicio
     str_drag_inicial = sprintf('H2:H%d',zero+1);
-    drag_inicial = xlsread(file_name,1, str_drag_inicial);
+    drag_inicial = 9.81 * xlsread(file_name,1, str_drag_inicial);
     
     %Leitura dos valores de thrust > 100%
     str_thrust_100 = sprintf('H%d:H%d',primeiro_100, primeiro_100 + cem);
-    thrust_100 = xlsread(file_name,1, str_thrust_100);
+    thrust_100 = 9.81 * xlsread(file_name,1, str_thrust_100);
     
     %Leitura dos dados de velocidade de escoamento a 100% de thrust
     str_veloc_100 = sprintf('J%d:J%d',primeiro_100, primeiro_100 + cem);
